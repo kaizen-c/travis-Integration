697d6d62b6bf9c36b1580de5aa9dde0969cba04a
ckodex <charith.de.alwis@ifsworld.com>
2021-03-03 15:39:56 +0530

    Update test_dice.py

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -5,7 +5,7 @@ from unittest import TestCase
 
 class TestDatetimeAPI(TestCase):
 	def test_test(self):
-		assert app.test() == "Works!!!"
+		assert app.test() == "Works!"
 	
 	#def test_dicePage(self):
 	#	data = requests.get("http://127.0.0.1:5000/voting?questionid=48d75c359ce4")
