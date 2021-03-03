94daf32dc219ff71d1fcfa15caf6a5f792f6399a
ckodex <charith.de.alwis@ifsworld.com>
2021-03-03 13:57:44 +0530

    deploy code

---
+++ tests/test_dice.py
@@ -0,0 +1,12 @@
+import unittest
+import requests
+import app
+from unittest import TestCase
+
+class TestDatetimeAPI(TestCase):
+	def test_test(self):
+		assert app.test() == "Works!"
+	
+	def test_dicePage(self):
+		data = requests.get("http://127.0.0.1:5000/test")
+		self.assertEqual(data.status_code, 200)
