0c8a75ebe5d52cdb72d29551d36efa9532572dfd
ckodex <charith.de.alwis@ifsworld.com>
2021-03-03 14:42:14 +0530

    Updating flask app

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
-from flask import Flask, render_template
-#Only for FLASK
+from flask import Flask
+from flask_cors import CORS, cross_origin
+import requests
+import json
+from flask import request
 import os
+
+
+def GetURL(VoteID):
+    QuestionsURI = 'https://api.mentimeter.com/questions/'+VoteID
+    ResultsURI = 'https://api.mentimeter.com/questions/'+VoteID+'/result'
+    # Adding User-Agent to avoid 403 forbidden error
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'}
+    # Get Question URI output and convert to Json
+    GetQuestions = requests.get(QuestionsURI, headers=headers)
+    Questions=GetQuestions.json()
+    # Get Results URI output and convert to Json
+    GetResults = requests.get(ResultsURI, headers=headers)
+    Results=GetResults.json()
+    #Create a dictionary and add Question and Results
+    resultDict = {
+      "question": Questions['question'],
+      "results": Results['results']
+    }
+    
+    return json.dumps(resultDict)
+    
 app = Flask(__name__)
+port = int(os.environ.get("PORT", 5000))
+CORS(app, support_credentials=True)
 
-#Dice Main page
-@app.route('/')
-def dice_page():
-	return render_template('dice_page.html')
+@app.route('/voting')
+@cross_origin(supports_credentials=True)
+def index():
+   #print (request.args.get('questionid'))
+   #return (request.args.get('questionid'))
+   return (GetURL(request.args.get('questionid')))
 
-#Testing to check if it works
-@app.route('/test')
-def test():
-    return "Works!"
 
 if __name__ == '__main__':
-
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True,host='0.0.0.0',port=port)
+    
+#
--- tests/test_dice.py
+++ tests/test_dice.py
@@ -4,11 +4,11 @@ import app
 from unittest import TestCase
 
 class TestDatetimeAPI(TestCase):
-	def test_test(self):
-		assert app.test() == "Works!"
+	#def test_test(self):
+	#	assert app.test() == "Works!"
 	
 	def test_dicePage(self):
-		data = requests.get("http://0.0.0.0:5000/")
+		data = requests.get("http://0.0.0.0:5000/voting")
 		self.assertEqual(data.status_code, 200)
 
 if __name__ == '__main__':
