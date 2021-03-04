504e39676f9b2f5708950ed132ae73e5b3bd2427
ckodex <charith.de.alwis@ifsworld.com>
2021-03-04 20:50:22 +0530

    URl Check

--- test_dice.py
+++ test_dice.py
@@ -8,10 +8,15 @@ class TestDatetimeAPI(TestCase):
 	def test_test(self):
 		assert app.test() == "Works!!!"
 	
-	def test_dicePage(self):
+	def test_GetURL(self):
 		ResultDict = app.GetURL('48d75c359ce4')
 		ResultDictJ = json.loads(ResultDict)
 		assert (ResultDictJ["question"]) =="Which is the best interactive presentation platform?"
+	
+	def test_URLConnectivity(self):
+		QuestionURI='https://travis-menti.herokuapp.com/voting?questionid=48d75c359ce4'
+		QURIResponse = requests.get(QuestionURI)
+		assert (str(QURIResponse.status_code)) =="200"
 
 if __name__ == '__main__':
     unittest.main()
